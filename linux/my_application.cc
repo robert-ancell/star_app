34acc98b8252bab99801713c86d3e5e361b08037
Robert Ancell <robert.ancell@gmail.com>
2023-12-02 22:52:58 +1300

    Replace boilerplate

--- linux/my_application.cc
+++ linux/my_application.cc
@@ -40,11 +40,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "star");
+    gtk_header_bar_set_title(header_bar, "Star");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "star");
+    gtk_window_set_title(window, "Star");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
