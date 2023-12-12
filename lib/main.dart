24009da2db33663fcff8023ef317e989dae6173b
Robert Ancell <robert.ancell@gmail.com>
2023-12-12 20:50:08 +1300

    Fix warning about SystemChrome.setEnabledSystemUIMode

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
 
   runApp(const StarApp());
